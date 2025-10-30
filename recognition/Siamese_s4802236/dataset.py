ddf974c979a8df92b280f6c1c71da6160f154660
Ishaan Sood <i.sood@student.uq.edu.au>
2025-10-30 16:51:30 +1000

    Added missing validation split and added random seed for future use

--- recognition/Siamese_s4802236/dataset.py
+++ recognition/Siamese_s4802236/dataset.py
@@ -10,6 +10,8 @@ import random, numpy as np
 import torch, torch.nn as nn
 from torchvision import transforms
 from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
+from PIL import Image
 ### FUNCTIONS ###
 def prepare_isic2020(clean_root: str | Path ="data", force: bool = False) -> tuple[Path, Path]:
     """ Prepares the ISIC 2020 224x224 dataset from Kaggle"""
@@ -65,13 +67,18 @@ def load_data(metadata_path: Path, images_folder: Path) -> tuple[list]:
     return np.array(image_paths), np.array(labels)
 
 def split_data(images: np.ndarray, labels: np.ndarray, seed: int = 42) -> tuple:
-    """ Splits data into 0.8 train, 0.2 test sets """
-    train_images, test_images, train_labels, test_labels = train_test_split(
+    """ Splits data into 0.8 train, 0.1 validation, 0.1 test sets """
+    train_imgs, test_imgs, train_labels, test_labels = train_test_split(
         images, labels, test_size=0.2, random_state=seed, stratify=labels
     )
-    return train_images, test_images, train_labels, test_labels
+    val_imgs, test_imgs, val_labels, test_labels = train_test_split(
+        test_imgs, test_labels, test_size=0.5, random_state=seed, stratify=test_labels
+    )
+    return (train_imgs, train_labels), (val_imgs, val_labels), (test_imgs, test_labels)
 
-meta_path, images_folder = prepare_isic2020()
 
-image_paths, labels = load_data(meta_path, images_folder)
+def set_seed(seed: int = 42):
+    random.seed(seed); np.random.seed(seed)
+    torch.manual_seed(seed); torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = True
 
